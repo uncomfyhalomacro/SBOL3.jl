247edb4cbba71107e90286faa7833632d618fe1f
Documenter.jl <documenter@juliadocs.github.io>
2022-06-19 01:08:02 +0000

    build based on da57c9e

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = SBOL3","category":"page"},{"location":"#SBOL3","page":"Home","title":"SBOL3","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for SBOL3.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [SBOL3]","category":"page"}]
+}
